ba3591c65f708e44db83bda85bae44f57b947f75
Oliver Nordh <oliver.nordh@proton.me>
2023-11-18 13:35:21 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: { 
+    files: ["*.html", "./src/**/*.rs"],
+  },
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        'smol': 'repeat(auto-fit, minmax(min(100%, var(--min)), 1fr))'
+      }
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        hoc: {
+         neutral: '#090606',
+         'base-100': '#f7f3f3',
+         primary: '#7ea6a9',
+         secondary: '#dac9c8',
+         accent: '#567e81',
+        },
+        dark: {
+         'neutral': '#e9f0f1',
+         'base-100': '#121a1c',
+         'primary': '#4c2946',
+         'secondary': '#061518',
+         'accent': '#907e5a',
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
