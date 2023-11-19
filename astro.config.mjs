c0829738ac01cf4d90adaa6186345ca1a7c72f98
Oliver Nordh <oliver.nordh@proton.me>
2023-11-19 18:15:55 +0100

    Så det funkar på github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://purung.github.io",
+  base: "/ma-psykolog-hemsida",
   integrations: [tailwind()]
 });
